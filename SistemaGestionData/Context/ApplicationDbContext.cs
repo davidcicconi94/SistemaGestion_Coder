cf56413e56a38a37875b80708d3f17b2065c056c
David Cicconi <david.cicconi94@gmail.com>
2024-02-08 23:05:42 -0300

    modify db context

--- SistemaGestionData/Context/ApplicationDbContext.cs
+++ SistemaGestionData/Context/ApplicationDbContext.cs
@@ -1,29 +1,24 @@
 ﻿using Microsoft.EntityFrameworkCore;
-using Microsoft.Extensions.Configuration;
 using SistemaGestionEntities;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace SistemaGestionData.Context
 {
-    public class ApplicationDbContext: DbContext
+    public class ApplicationDbContext : DbContext
     {
-        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
-        {
-        }
+        public ApplicationDbContext() { }
+
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options) { }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder) { }
 
         protected override void OnModelCreating(ModelBuilder modelBuilder)
         {
-
             modelBuilder.Entity<Usuario>().HasKey(p => p.Id);
-
-
 
             base.OnModelCreating(modelBuilder);
         }
+
         public virtual DbSet<Usuario> Usuarios { get; set; }
     }
 }
